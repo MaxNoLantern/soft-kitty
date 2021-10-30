56dc0bfa02a0c9632db3fe9d1736f9eab6d12333
Max <mxm.barreau@gmail.com>
2021-10-30 21:58:04 +0200

    Intialisation de l'application

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      color="primary"
+      class="white--text"
+    >
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Soft kitty',
+    }
+  },
+}
+</script>
